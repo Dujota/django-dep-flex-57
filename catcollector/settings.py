a71e5774c05c8f7f9030bc362449565c8f655ac6
Denis S Dujota <dujota@Deniss-MacBook-Pro.local>
2023-03-15 20:44:32 -0500

    prep django app for heroku deploy

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 
+WSGI_APPLICATION = 'catcollector.wsgi.application'
+# catcollector is the project name
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -123,3 +127,6 @@ STATIC_URL = '/static/'
 # Add this variable to specify where successful logins should redirect to
 LOGIN_REDIRECT_URL = '/cats/'
 LOGOUT_REDIRECT_URL = '/'
+
+import django_heroku
+django_heroku.settings(locals())
